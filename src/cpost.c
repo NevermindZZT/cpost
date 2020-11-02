4df2f691bd26c080b8c96e7a9029d6001a700a71
Letter <nevermindzzt@gmail.com>
2020-11-02 20:10:04 +0800

    cpost

---
+++ src/cpost.c
@@ -0,0 +1,108 @@
+/**
+ * @file cpost.c
+ * @author Letter (nevermindzzt@gmail.com)
+ * @brief c post
+ * @version 1.0.0
+ * @date 2020-10-31
+ * 
+ * @copyright (c) 2020 Letter
+ * 
+ */
+#include "cpost.h"
+
+
+CpostHandler cposhHandlers[CPOST_MAX_HANDLER_SIZE] = {0};
+
+/**
+ * @brief cpost 添加 handler
+ * 
+ * @param time 执行时间
+ * @param handler handler
+ * 
+ * @return size_t 0 添加成功 -1 添加失败
+ */
+size_t cpostAddHandler(size_t time, void *handler)
+{
+    for (size_t i = 0; i < CPOST_MAX_HANDLER_SIZE; i++)
+    {
+        if (cposhHandlers[i].handler == NULL)
+        {
+            cposhHandlers[i].time = time;
+            cposhHandlers[i].handler = (void (*)(void))handler;
+            return 0;
+        }
+    }
+    return -1;
+}
+
+/**
+ * @brief post handler
+ * 
+ * @param handler handler 
+ * 
+ */
+void cpost(void *handler)
+{
+    cpostAddHandler(0, handler);
+}
+
+/**
+ * @brief 延迟 post handler
+ * 
+ * @param handler handler
+ * @param delay 延时时间(tick)
+ * 
+ */
+void cpostDelay(void *handler, size_t delay)
+{
+    cpostAddHandler(CPOST_GET_TICK() + delay, handler);
+}
+
+/**
+ * @brief 移除handler
+ * 
+ * @param handler handler
+ * 
+ */
+void cpostRemove(void *handler)
+{
+    for (size_t i = 0; i < CPOST_MAX_HANDLER_SIZE; i++)
+    {
+        if (cposhHandlers[i].handler == handler)
+        {
+            cposhHandlers[i].handler = NULL;
+        }
+    }
+}
+
+/**
+ * @brief 移除所有handler
+ * 
+ */
+void cpostRemoveAll(void)
+{
+    for (size_t i = 0; i < CPOST_MAX_HANDLER_SIZE; i++)
+    {
+        cposhHandlers[i].handler = NULL;
+    }
+}
+
+/**
+ * @brief cpost 处理
+ * 
+ */
+void cpostProcess(void)
+{
+    for (size_t i = 0; i < CPOST_MAX_HANDLER_SIZE; i++)
+    {
+        if (cposhHandlers[i].handler)
+        {
+            if (cposhHandlers[i].time == 0 || CPOST_GET_TICK() >= cposhHandlers[i].time)
+            {
+                cposhHandlers[i].handler();
+                cposhHandlers[i].handler = NULL;
+            }
+        }
+    }
+}
+
---
+++ src/cpost.h
@@ -0,0 +1,43 @@
+/**
+ * @file cpost.h
+ * @author Letter (nevermindzzt@gmail.com)
+ * @brief c post
+ * @version 1.0.0
+ * @date 2020-10-31
+ * 
+ * @copyright (c) 2020 Letter
+ * 
+ */
+#ifndef __CPOST_H__
+#define __CPOST_H__
+
+#define     CPOST_VERSION               "1.0.0"
+
+/**
+ * @brief 最大handler数量
+ */
+#define     CPOST_MAX_HANDLER_SIZE      16
+
+/**
+ * @brief 获取系统tick函数
+ */
+#define     CPOST_GET_TICK()            0
+
+typedef struct
+{
+    size_t time;
+    void (*handler)(void);
+} CpostHandler;
+
+void cpost(void *handler);
+
+void cpostDelay(void *handler, size_t delay);
+
+void cpostProcess(void);
+
+void cpostRemove(void *handler);
+
+void cpostRemoveAll(void);
+
+#endif
+
